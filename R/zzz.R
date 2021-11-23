a49235b7c8fd5014f6accce6ce09fe694e235921
jorainer <johannes.rainer@gmail.com>
2021-11-23 17:03:51 +0100

    feat: load library on startup

---
+++ R/zzz.R
@@ -0,0 +1,14 @@
+#' @importFrom opentimsr setup_bruker_so
+#'
+#' @noRd
+.onLoad <- function(libname, pkgname) {
+    so <- getOption("TIMSTOF_LIB", default = NA)
+    if (is.na(so))
+        so <- Sys.getenv("TIMSTOF_LIB", unset = NA)
+    if (is.na(so))
+        message("Location of the TimsTOF library not known.\nYou need to ",
+                "use 'opentimsr::setup_bruker_so' to set/load this library ",
+                "in order to be able to read TimsTOF files. See the package ",
+                "vignette for more information.")
+    else setup_bruker_so(so)
+}
