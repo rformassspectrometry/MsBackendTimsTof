7e834b6a56eedaf5f12be7f488d4e5651ba2559c
jorainer <johannes.rainer@gmail.com>
2022-03-29 09:57:44 +0200

    ci: disable Windows

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,4 @@
 library("testthat")
-library("MsBackendTimsTof")
-
 so_folder <- tempdir()
 library(opentimsr)
 so_file <- download_bruker_proprietary_code(so_folder)
@@ -8,6 +6,7 @@ setup_bruker_so(so_file)
 path_d_folder <- system.file("ddaPASEF.d",
                              package = "MsBackendTimsTof")
 
+library("MsBackendTimsTof")
 be <- backendInitialize(new("MsBackendTimsTof"), rep(path_d_folder, 2))
 
 test_check("MsBackendTimsTof")
