6265105c88f9130f60fd7b7b194b1af83fd449dd
jorainer <johannes.rainer@gmail.com>
2022-03-29 09:38:31 +0200

    tests: don't use wget for downloading

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library("MsBackendTimsTof")
 
 so_folder <- tempdir()
 library(opentimsr)
-so_file <- download_bruker_proprietary_code(so_folder, method = "wget")
+so_file <- download_bruker_proprietary_code(so_folder)
 setup_bruker_so(so_file)
 path_d_folder <- system.file("ddaPASEF.d",
                              package = "MsBackendTimsTof")
